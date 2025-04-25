7c379733a7099ad0492c1ecdf6a25826790014f8
noahra <noah.rahimzadagan@protonmail.com>
2025-04-25 20:39:44 +0200

    scan multiple lines and add mandelbrot set

--- internal/interpreterutils/repl.go
+++ internal/interpreterutils/repl.go
@@ -3,22 +3,31 @@ package interpreterutils
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 func RunRepl() {
-	scanner := bufio.NewScanner(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Printf("\nBF-REPL> ")
-		if scanner.Scan() {
-			input := scanner.Text()
-			if input == "exit" {
+		var lines []string
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			if len(strings.TrimSpace(line)) == 0 {
 				break
 			}
-			tokens := interpretInput(input)
-			fmt.Println()
-			output, _ := executeBrainfuck(tokens)
-			fmt.Println(output)
+			lines = append(lines, line)
 		}
+		input := strings.Join(lines, "")
+		tokens := interpretInput(input)
+		fmt.Println()
+		output, _ := executeBrainfuck(tokens)
+		fmt.Println(output)
+
 	}
 }
